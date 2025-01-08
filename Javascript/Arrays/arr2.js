509dae359a59370c096d4057b6d9e42f381d4c5a
wendersonalexandre02 <136288467+wendersonalexandre@users.noreply.github.com>
2025-01-08 18:01:10 -0300

    aprendendo sobre os metódos .concat (), .join() e .tostring()

--- Javascript/Arrays/arr2.js
+++ Javascript/Arrays/arr2.js
@@ -1,5 +1,7 @@
 let arr =[10, 11, 3, 4 , 61, 45, 11]
 
+console.log(arr.indexOf(3)) // Vai me retornar 2, pois o valor 3 esta na posição 2 do array
+
 console.log(arr.lastIndexOf(61)) // Vai me retornar 4, pois o valor 61 esta na posição 4 do array
 console.log(arr.lastIndexOf(632)) // Vai me retornar -1, pois o valor não existe no array
 
@@ -7,10 +9,10 @@ console.log(arr.lastIndexOf(632)) // Vai me retornar -1, pois o valor não exist
 console.log(arr.includes(3)) // me retorna true, pois o valor 3 existe no array
 console.log(arr.includes(123)) // tambem vai me retornar false, pois o valor não existe no array
 
- console.log(arr.find(function(el){
+ console.log(arr.find(function(el){  // Vai me retornar o primeiro valor que for maior que 10
     return el > 10
  }))
 
- console.log(arr.findIndex(function(el){
+ console.log(arr.findIndex(function(el){ // Vai me retornar a posição do primeiro valor que for maior que 10
     return el > 10
  }))
---
+++ Javascript/Arrays/arr3.js
@@ -0,0 +1,9 @@
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = [6, 7, 8, 9, 10];
+
+console.log(arr1.toString())
+
+console.log(arr2.join('-'))
+
+let arr3 = arr1.concat(arr2,21,32,43,["ola", "mundo"])
+console.log(arr3)
