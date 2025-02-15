baeb66b68a2caa7bd3f6cb29ab779faef00fdc4e
wendersonalexandre02 <136288467+wendersonalexandre@users.noreply.github.com>
2025-02-15 16:38:43 -0300

    aprendendo sobre metodos call e apply

---
+++ Javascript/Arrays/Desafio_ES5.js
@@ -0,0 +1,31 @@
+ function sum(){
+     const numbers = []; /*
+     for (let i = 0; i < arguments.length; i++){
+        numbers.push(arguments[i])
+     }
+     console.log(numbers)  */
+
+      Array.prototype.forEach.call(arguments, function(argument){
+        numbers.push(argument)
+      })
+
+      return numbers.reduce(function(sum,atual){
+        return sum + atual;
+    },0);
+
+ }
+
+
+ 
+
+ function avarege(){
+    const soma = sum.apply(null,arguments );
+    console.log(soma)
+    return soma / arguments.length
+ };
+
+ console.log(avarege(1,2,3,4,5));
+ 
+ //sum.call(null,1,2,3,4,5)
+
+ //sum.apply(null, [1,2,3,4,5])
